e6583e5dc171320a49088e0830b719c2c9b90973
Aleksandr Rublev <aleksandr.rublev@itl.team>
2023-09-15 18:15:14 +0300

    PT-1 - Refactor main.py

--- N1/src/main.py
+++ N1/src/main.py
@@ -1,7 +1,11 @@
 import argparse
 import sys
+
+from typing import Optional
+
 from CalcRating import CalcRating
 from SuccessRating import SuccessRating
+from SuccessRating import SuccessType
 
 from DataReader import DataReader
 from TextDataReader import TextDataReader
@@ -21,6 +25,14 @@ def get_path_from_arguments(args) -> str:
 
     args = parser.parse_args(args)
     return args.path
+
+
+def get_successful_student(success_rating: SuccessType) -> Optional[str]:
+    for student_name, success_info in success_rating.items():
+        if success_info['Success']:
+            return student_name
+
+    return None
 
 
 def main():
@@ -45,15 +57,11 @@ def main():
     print('Calc rating: ', calc_rating)
     print('Success rating: ', success_rating)
 
-    successful_student_exists = False
+    successful_student_name = get_successful_student(success_rating)
 
-    for student_name, success_info in success_rating.items():
-        if success_info['Success']:
-            successful_student_exists = True
-            print('Successful student: ', student_name)
-            break
-
-    if not successful_student_exists:
+    if successful_student_name:
+        print('Successful student: ', successful_student_name)
+    else:
         print('There are no successful students')
 
 
