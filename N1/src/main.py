9b277d81a6fa91a2d05be53c0f8b30cf1230e9a6
Aleksandr Rublev <aleksandr.rublev@itl.team>
2023-09-15 08:12:24 +0300

    PT-1 - Change main function

--- N1/src/main.py
+++ N1/src/main.py
@@ -1,7 +1,11 @@
 import argparse
 import sys
 from CalcRating import CalcRating
+from SuccessRating import SuccessRating
+
+from DataReader import DataReader
 from TextDataReader import TextDataReader
+from JsonDataReader import JsonDataReader
 
 
 def get_path_from_arguments(args) -> str:
@@ -22,14 +26,35 @@ def get_path_from_arguments(args) -> str:
 def main():
     path = get_path_from_arguments(sys.argv[1:])
 
-    reader = TextDataReader()
+    reader: DataReader = None
+
+    if path.endswith('.txt'):
+        reader = TextDataReader()
+    elif path.endswith('.json'):
+        reader = JsonDataReader()
+    else:
+        raise ValueError('Incorrect file type provided!')
+
     students = reader.read(path)
 
     print('Students: ', students)
 
-    rating = CalcRating(students).calc()
+    calc_rating = CalcRating(students).calc()
+    success_rating = SuccessRating(students).calc()
 
-    print('Rating: ', rating)
+    print('Calc rating: ', calc_rating)
+    print('Success rating: ', success_rating)
+
+    successful_student_exists = False
+
+    for student_name, success_info in success_rating.items():
+        if success_info['Success']:
+            successful_student_exists = True
+            print('Successful student: ', student_name)
+            break
+
+    if not successful_student_exists:
+        print('There are no successful students')
 
 
 if __name__ == '__main__':
